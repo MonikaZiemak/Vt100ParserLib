d0b17e5bb2ed6933713ad3c3b5efc7110846048f
Monika Ziemak <wwx29594@student.warszawa.merito.pl>
2025-08-05 15:04:47 +0200

    dodano example

---
+++ example/Vt100ParserApp/Vt100ParserApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Vt100ParserLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte[] inputData = new byte[]
+        {
+            0x1B, 0x5B, 0x33, 0x31, 0x6D, // ESC[31m
+            (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o',
+            0x1B, 0x5B, 0x30, 0x6D         // ESC[0m
+        };
+
+        Vt100Parser parser = new Vt100Parser();
+        string plain = parser.Parse(inputData);
+        string debug = parser.ParseWithSequences(inputData);
+
+        Console.WriteLine("Po usunięciu sekwencji:");
+        Console.WriteLine(plain);
+        Console.WriteLine("\nZ debugiem:");
+        Console.WriteLine(debug);
+    }
+}
